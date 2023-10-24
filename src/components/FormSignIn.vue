460bc784c5ddd7940e318b58dd1c561bb51cc995
Gustavo Meira <gmeira.antonio@gmail.com>
2023-10-24 12:42:37 -0300

    feat: create FormSignIn

---
+++ src/components/FormSignIn.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+  
+</script>
+
+<template>
+  <form class="form-control">
+    <h2 class="text-3xl text-center text-black">Entrar</h2>
+    <label class="label">
+      <span class="label-text text-gray-800">Email</span>
+    </label>
+    <input class="input input-primary" />
+    <label class="label">
+      <span class="label-text text-gray-800">Senha</span>
+    </label>
+    <input class="input input-primary" />
+    <span class="text-gray-700 mt-2">
+      Ainda não tem uma conta? 
+      <a class="link" href="#">Cadastre-se</a>
+    </span>
+    <button class="btn btn-primary mt-4">Entrar</button>
+  </form>
+</template>
+
+<style scoped>
+</style>
