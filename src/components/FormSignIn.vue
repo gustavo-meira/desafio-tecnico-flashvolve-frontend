3b6da7800f9837e2aba96b80aa6d3a97dcae65ef
Gustavo Meira <gmeira.antonio@gmail.com>
2023-10-24 12:58:13 -0300

    feat: creating minimal validating to FormSignIn

--- src/components/FormSignIn.vue
+++ src/components/FormSignIn.vue
@@ -1,18 +1,47 @@
 <script setup lang="ts">
-  
+  import { computed, reactive } from 'vue';
+
+  const state = reactive({
+    email: '',
+    password: '',
+  });
+
+  const errors = reactive({
+    email: '',
+    password: '',
+  });
+
+  const classInputEmail = computed(() => {
+    return errors.email ? 'input input-error' : 'input input-primary';
+  });
+
+  const classInputPassword = computed(() => {
+    return errors.password ? 'input input-error' : 'input input-primary';
+  });
+
+  const onSubmit = () => {
+    if (state.email === '') return errors.email = 'Email é obrigatório';
+    else if (state.email.length >= 1) errors.email = '';
+    if (state.password === '') return errors.password = 'Senha é obrigatória';
+    else if (state.password.length >= 1) errors.password = '';
+
+    console.log(state);
+  };
 </script>
 
 <template>
-  <form class="form-control">
+  <form @submit.prevent="onSubmit" class="form-control">
     <h2 class="text-3xl text-center text-black">Entrar</h2>
     <label class="label">
       <span class="label-text text-gray-800">Email</span>
     </label>
-    <input class="input input-primary" />
+    <input type="email" v-model="state.email" :class="classInputEmail" />
+    <span class="text-red-500 text-sm">{{ errors.email }}</span>
     <label class="label">
       <span class="label-text text-gray-800">Senha</span>
     </label>
-    <input class="input input-primary" />
+    <input type="password" v-model="state.password" :class="classInputPassword" />
+    <span class="text-red-500 text-sm">{{ errors.password }}</span>
     <span class="text-gray-700 mt-2">
       Ainda não tem uma conta? 
       <a class="link" href="#">Cadastre-se</a>
