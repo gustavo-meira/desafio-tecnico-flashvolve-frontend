b2cdf86727b68c9e55f3b4b624180c02eb85d548
Gustavo Meira <gmeira.antonio@gmail.com>
2023-10-24 13:03:57 -0300

    feat: create page SignIn

---
+++ src/views/SignInView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+  import FormContainer from '../components/FormContainer.vue';
+  import FormSignIn from '../components/FormSignIn.vue';
+</script>
+
+<template>
+  <FormContainer>
+    <FormSignIn />
+  </FormContainer>
+</template>
