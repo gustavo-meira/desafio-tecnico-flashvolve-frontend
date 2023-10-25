16ff4eab931dad8fe1c37329f2c4a5a2deb18e58
Gustavo Meira <gmeira.antonio@gmail.com>
2023-10-25 06:50:51 -0300

    feat: create an api to routes that need auth

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,22 @@
 import axios from 'axios';
+import { getToken } from '../services/token';
 
-export const api = axios.create({
+const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
+
+const apiAuth = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});
+
+apiAuth.interceptors.request.use((config) => {
+  const token = getToken();
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+export { apiAuth, api };
