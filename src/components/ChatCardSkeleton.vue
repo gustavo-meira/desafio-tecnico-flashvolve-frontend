87436d72024648dcca9df1df418db404bb6d3d91
Gustavo Meira <gmeira.antonio@gmail.com>
2023-10-25 07:06:22 -0300

    feat: create a component for ChatCardSkeleton

---
+++ src/components/ChatCardSkeleton.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="card w-96 h-32 bg-base-200 rounded-none divide-y shadow-xl">
+    <div class="card-body">
+      <h2 class="card-title animate-pulse capitalize bg-gray-300 h-4 w-full"></h2>
+      <p class="text-ellipsis animate-pulse bg-gray-300 h-2 w-full"></p>
+    </div>
+  </div>
+</template>
