1560898ac86f120dfddbf77561a122c923a96092
Gustavo Meira <gmeira.antonio@gmail.com>
2023-10-25 07:05:44 -0300

    feat: create a component for ChatCard

---
+++ src/components/ChatCard.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { Chat } from '../types/chat';
+
+  defineProps<{
+    chat: Chat;
+  }>();
+</script>
+
+<template>
+  <router-link :to="`/chat/${chat.id}`" class="card w-96 h-32 bg-base-200 rounded-none divide-y shadow-xl">
+    <div class="card-body">
+      <h2 class="card-title capitalize">{{ chat.name }}</h2>
+      <p class="text-ellipsis line-clamp-2">Ultima mensagem: {{ chat.lastMessage }}</p>
+    </div>
+  </router-link>
+</template>
