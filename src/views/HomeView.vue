6c9ef4aeec72c02d4ecc9929025cf305a440acec
Gustavo Meira <gmeira.antonio@gmail.com>
2023-10-25 16:55:12 -0300

    feat: add link to bot on HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,8 +6,11 @@
 <template>
   <ChatContainer>
     <Chats />
-    <div class="flex flex-grow justify-center items-center">
+    <div class="flex flex-grow justify-center items-center flex-col">
       <h2 class="text-center text-3xl">Selecione um chat para começar a conversar.</h2>
+      <h2 class="text-center text-2xl">Ou envie uma mensagem para o 
+        <a class="link" target="_blank" href="https://t.me/des_tec_flashvolve_bot">Bot.</a>
+      </h2>
     </div>
   </ChatContainer>
 </template>
