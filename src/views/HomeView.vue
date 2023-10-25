fef028d266ab6707b81b928e600432f13291b002
Gustavo Meira <gmeira.antonio@gmail.com>
2023-10-25 07:10:56 -0300

    feat: create Home view

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+  import ChatContainer from '../components/ChatContainer.vue';
+  import Chats from '../components/Chats.vue';
+</script>
+
+<template>
+  <ChatContainer>
+    <Chats />
+    <div class="flex flex-grow justify-center items-center">
+      <h2 class="text-center text-3xl">Selecione um chat para começar a conversar.</h2>
+    </div>
+  </ChatContainer>
+</template>
